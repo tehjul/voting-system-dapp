20c351347dad262175551855c60bbd1b53c6e178
tehjul <sibillej@gmail.com>
2022-11-08 12:14:00 +0100

    Add current administrator

--- client/src/components/Header/Desc.jsx
+++ client/src/components/Header/Desc.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
 import { useEth } from "../../contexts/EthContext";
 
 function Desc() {
-  const { state: { accounts } } = useEth();
+  const { state: { accounts, contract } } = useEth();
+  const [owner, setOwner] = useState("");
+
+  useEffect(() => {
+    async function fetchOwner() {
+      try {
+        const currentOwner = await contract.methods.owner().call();
+        setOwner(currentOwner);
+      } catch (err) {
+        setOwner("");
+      }
+    }
+    fetchOwner();
+  }, [owner, contract]);
+
   return (
     <>
+    <h3>Current administrator is : {owner}</h3>
       {
         accounts ?
           <p>Connected with wallet {accounts[0]}</p>
