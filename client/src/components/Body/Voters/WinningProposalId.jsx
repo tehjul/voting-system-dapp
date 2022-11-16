06ee5d6c83f47aa4cc20195f73e8ca2d996b8009
tehjul <sibillej@gmail.com>
2022-11-16 16:12:39 +0100

    Show winner only on click

--- client/src/components/Body/Voters/WinningProposalId.jsx
+++ client/src/components/Body/Voters/WinningProposalId.jsx
@@ -2,17 +2,28 @@ import { useState } from "react";
 import useEth from "../../../contexts/EthContext/useEth";
 
 function WinningProposalId() {
-  const { state: { contract } } = useEth();
-  const [winningProposalId, setWinningProposalId] = useState("There's no winner at the moment")
+  const { state: { contract, currentStatus } } = useEth();
+  const [winningProposalId, setWinningProposalId] = useState("");
+  const LAST_PHASE_STATUS = 5;
+
   const getWinningProposal = async () => {
+    setWinningProposalId("");
     const value = await contract.methods.winningProposalID().call();
-    if (parseInt(value) !== 0) setWinningProposalId(value);
+    if (parseInt(value) !== 0) {
+      if (parseInt(currentStatus) === LAST_PHASE_STATUS) {
+        setWinningProposalId("Final winner is proposal #" + value + " !");
+      } else {
+        setWinningProposalId("Temporary winner is proposal #" + value);
+      }
+    } else {
+      setWinningProposalId("There's no winner at the moment");
+    }
   };
 
   return (
     <>
       <button onClick={getWinningProposal}>Winning proposal ID</button>
-      <code>{winningProposalId}</code>
+      {winningProposalId && <code>{winningProposalId}</code>}
     </>
   );
 }
